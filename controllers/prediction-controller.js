e45d17c1aacc2dbcbcdf8e7bb427c2d4ad93fbbe
mnmpk <mnmpkm@gmail.com>
2024-04-20 23:39:11 +0800

    2 routes

--- controllers/prediction-controller.js
+++ controllers/prediction-controller.js
@@ -27,17 +27,26 @@ const routes = [
     [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [8, 1], [9, 1]],
     [[2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [8, 1], [9, 1]],
     [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1]],
-    //[[0, 0], [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    //different route
+    [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7]],
+    [[5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    [[7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8]],
+    [[6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    [[6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    [[3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    [[5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8], [7, 8], [7, 9], [8, 9], [9, 9]],
+    [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5]],
+    [[0, 0], [1, 0], [1, 1], [2, 1], [3, 1], [4, 1], [4, 2], [5, 2], [6, 2], [7, 2], [7, 3], [7, 4], [6, 4], [6, 5], [5, 5], [5, 6], [5, 7], [6, 7], [6, 8]],
 ]
 
-const r = routes[0];
 let indices = [];
-//routes.forEach((r, i) => {
+routes.forEach((r, i) => {
 r.forEach((p, i) => {
     const v = (p[1] * 10) + p[0];
     if (indices.indexOf(v) == -1) indices.push(v);
 });
-//});
+});
 console.log(indices);
 
 const predict = (async (req, res) => {
@@ -48,20 +57,18 @@ const predict = (async (req, res) => {
         model = await train();
     }
     if (model) {
-        let result = await generatePath(model, indices, req.body.path.map(p => parseInt(p)), req.body.length, 0.1);
+        let result = await generatePath(model, indices, req.body.path.map(p => parseInt(p)), req.body.length, 0.5);
         result = result.map(v => [v % 10, Math.floor(v / 10)]);
         res.send(result);
     }
 })
 
 async function train() {
-    //routes.forEach((r, i) => {
-    //});
     //Find all valid point index from route first
     const routeExampleSize = 100;
     const pointLen = indices.length;
-    const rememberLen = 4;
-    const model = createModel(rememberLen, pointLen, 32);
+    const rememberLen = 5;
+    const model = createModel(rememberLen, pointLen, 64);
     compileModel(model, 1e-2);
 
     // Train the model.
@@ -69,6 +76,7 @@ async function train() {
         routes.length, rememberLen, pointLen]);
     let label = new tf.TensorBuffer([routes.length, pointLen]);
 
+    routes.forEach((r, ri) => {
     let randomList = [];
     for (let i = 0;
         i < r.length - rememberLen;
@@ -78,44 +86,21 @@ async function train() {
     tf.util.shuffle(randomList);
     console.log(randomList);
 
-    for (let i = 0; i < routeExampleSize; i++) {
+    for (let i = ri*routeExampleSize; i < ri*routeExampleSize+routeExampleSize; i++) {
         const startIndex = randomList[i % randomList.length];
         for (let j = 0; j < rememberLen; j++) {
             const p = startIndex + j;
-            console.log(i, j, startIndex + j, p, [p % 10, Math.floor(p / 10)]);
-            input.set(1, i, j, startIndex + j);
+            console.log(i, j, p, indices[p], [indices[p] % 10, Math.floor(indices[p] / 10)]);
+            input.set(1, i, j, p);
         }
         const t = startIndex + rememberLen;
-        console.log("target", startIndex + rememberLen, t, [t % 10, Math.floor(t / 10)]);
-        label.set(1, i, startIndex + rememberLen);
+        console.log("target", t, indices[t], [indices[t] % 10, Math.floor(indices[t] / 10)]);
+        label.set(1, i, t);
     }
+});
     //input.toTensor().data().then(data => console.log(data));
     //label.toTensor().data().then(data => console.log(data));
     //console.log(input, label);
-
-    /*console.log("indices", indices);
-    routes.forEach((r, i) => {
-        let randomList = [];
-        for (let i = 0;
-            i < r.length - rememberLen - 1;
-            i++) {
-            randomList.push(i);
-        }
-        tf.util.shuffle(randomList);
-        console.log("randomList", randomList);
-        randomList.forEach((pi) => {
-            let startIndex = pi;
-            for (let j = 0; j < rememberLen; j++) {
-                const p = indices[startIndex + j];
-                console.log(i, j, startIndex + j, p, [Math.floor(p / 10), p % 10]);
-                input.set(1, i, j, indices[startIndex + j]);
-            }
-            const t = indices[startIndex + rememberLen];
-            console.log("target", startIndex + rememberLen, t, [Math.floor(t / 10), t % 10]);
-            label.set(1, i, indices[startIndex + rememberLen]);
-        });
-        console.log(input, label);
-    });*/
 
     await fitModel(
         model, input.toTensor(), label.toTensor(), 1000, 128, 0.0625,
